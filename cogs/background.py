bfd2b8a0eb6b9e0ac221b181c10ad105b4e007f6
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-27 18:36:06 -0400

    server init

--- cogs/background.py
+++ cogs/background.py
@@ -13,7 +13,8 @@ class Background(commands.Cog):
     @tasks.loop(seconds = 5)
     async def printer(self):
         self.up_time += 5
-        await self.client.change_presence(status = discord.Status.online, activity = discord.Game(f".help | {str(convertTime(self.up_time))}"))
+        newtime = str(convertTime(self.up_time))
+        await self.client.change_presence(status = discord.Status.online, activity = discord.Game(f".help | {newtime}"))
 
     @printer.before_loop
     async def before_printer(self):
