9bb19b79be5e89f7d61eaed1c03b1a9da4569727
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-29 09:28:29 -0400

    added bot owner perms

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,26 +7,26 @@ class Moderation(commands.Cog):
         self.client = client
 
     @commands.command()
-    @commands.has_permissions(manage_messages = True)
+    @commands.check_any(commands.is_owner(), commands.has_permissions(manage_messages = True))
     async def clear(self, ctx, amount = 1):
         deleted_msgs = await ctx.channel.purge(limit = amount + 1)
         num_of_msgs = len(deleted_msgs) - 1
         message = await ctx.send(content = str(num_of_msgs) + " messages were deleted", delete_after = 2)
 
     @commands.command()
-    @commands.has_permissions(kick_members = True)
+    @commands.check_any(commands.is_owner(), commands.has_permissions(kick_members = True))
     async def kick(self, ctx, member: discord.Member, *, reason = None):
         await member.kick(reason = reason)
         await ctx.send(str(member) + " was kicked")
 
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    @commands.check_any(commands.is_owner(), commands.has_permissions(ban_members = True))
     async def ban(self, ctx, member: discord.Member, *, reason = None):
         await member.ban(reason = reason)
         await ctx.send(str(member) + " was banned")
 
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    @commands.check_any(commands.is_owner(), commands.has_permissions(ban_members = True))
     async def unban(self, ctx, *, member):
         bannedUsers = await ctx.guild.bans()
         memberName, memberDiscriminator = member.split("#")
